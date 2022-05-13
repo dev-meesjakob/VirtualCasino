32a901fde311476ecd71d6f85afde0ac42a8ebfd
Jakob Mees <meesjakob.dev@gmail.com>
2022-05-13 08:09:32 +0200

    start stage on button

--- src/main/java/com/toja/virtualcasino/VirtualCasinoController.java
+++ src/main/java/com/toja/virtualcasino/VirtualCasinoController.java
@@ -1,7 +1,12 @@
 package com.toja.virtualcasino;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class VirtualCasinoController {
     @FXML
@@ -10,5 +15,15 @@ public class VirtualCasinoController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void startSlotMachine() throws IOException {
+        Stage slotMachineStage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(VirtualCasino.class.getResource("slotmachine-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        slotMachineStage.setTitle("Hello!");
+        slotMachineStage.setScene(scene);
+        slotMachineStage.show();
     }
 }
