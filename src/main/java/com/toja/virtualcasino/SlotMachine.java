27408c5f59193a807cad54240cd0ceafb404f492
torge.neuendorf <torge.neuendorf@iserv-gis.de>
2022-05-25 13:05:31 +0200

    Slot Machine grundlegendes

--- src/main/java/com/toja/virtualcasino/SlotMachine.java
+++ src/main/java/com/toja/virtualcasino/SlotMachine.java
@@ -1,4 +1,51 @@
 package com.toja.virtualcasino;
 
+import java.util.ArrayList;
+
 public class SlotMachine {
+    public static ArrayList<Reel> machine = new ArrayList<>();
+
+    public static void machineCreate() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add("" + (char) (i+65));
+        }
+        for (int i = 0; i < 5; i++) {
+            Reel temp = new Reel(list);
+            machine.add(temp);
+        }
+
+    }
+
+    public static ArrayList<String> spin(int b) {
+        VirtualCasinoController.setCurrAmount(VirtualCasinoController.getCurrAmount() - b);
+
+        ArrayList<String> list= new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.addAll(machine.get(i).getFrontIcons());
+        }
+
+        winCheck();
+
+        return list;
+    }
+
+    public static void winCheck() {
+        for (int i = 0; i < 3; i++) {
+            int x = 0;
+            if (machine.get(0).getFrontIcons().get(i) == machine.get(1).getFrontIcons().get(i) || machine.get(1).getFrontIcons().get(i) == machine.get(2).getFrontIcons().get(i)) {
+                if (machine.get(2).getFrontIcons().get(i) == machine.get(3).getFrontIcons().get(i)) {
+                    if (machine.get(3).getFrontIcons().get(i) == machine.get(4).getFrontIcons().get(i)) {
+                        x = 10;
+                    } else {
+                        x = 3;
+                    }
+                } else {
+                    x = 1;
+                }
+            }
+
+        }
+    }
+
 }
--- src/main/java/com/toja/virtualcasino/SlotMachineController.java
+++ src/main/java/com/toja/virtualcasino/SlotMachineController.java
@@ -3,6 +3,11 @@ package com.toja.virtualcasino;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
 
 public class SlotMachineController {
 
@@ -10,15 +15,45 @@ public class SlotMachineController {
     private Label amountLabel;
     @FXML
     private Button playBtn;
+    @FXML
+    private ArrayList<TextArea> reels = new ArrayList<>();
+    @FXML
+    private Pane slotPane;
+    @FXML
+    private TextField betFld;
 
     public void initialize() {
+        SlotMachine.machineCreate();
         amountLabel.setText(VirtualCasinoController.getCurrAmount() + "$");
         if (VirtualCasinoController.getCurrAmount() == 0) {
             playBtn.setDisable(true);
         }
+        for (int i = 0; i < 5; i++) {
+            TextArea temp = new TextArea();
+            reels.add(temp);
+            reels.get(i).setEditable(false);
+            reels.get(i).setLayoutY(43.0);
+            reels.get(i).setPrefHeight(120.0);
+            reels.get(i).setPrefWidth(90.0);
+            reels.get(i).setLayoutX(40 + 95*i);
+            slotPane.getChildren().add(reels.get(i));
+        }
     }
 
     public void playSlot() {
-        
+        if (!betFld.getText().equals("")) {
+            int bet = Integer.parseInt(betFld.getText());
+            ArrayList<String> allIcons = SlotMachine.spin(bet);
+
+            int zaehler = 0;
+            for (int i = 0; i < 5; i++) {
+                reels.get(i).clear();
+                for (int x = 0; x < 3; x++) {
+                    reels.get(i).appendText(allIcons.get(zaehler) + "\n");
+                    zaehler++;
+                }
+            }
+            amountLabel.setText(VirtualCasinoController.getCurrAmount() + "$");
+        }
     }
 }
