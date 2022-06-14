642a2a47d6d181c457edd48e6956a3c97df2d950
Torge-GitHub <98641337+Torge-GitHub@users.noreply.github.com>
2022-06-14 16:48:42 +0200

    Hoffe, ich hab nix zerstört...

--- src/main/java/com/toja/virtualcasino/WheelFortController.java
+++ src/main/java/com/toja/virtualcasino/WheelFortController.java
@@ -1,5 +1,8 @@
 package com.toja.virtualcasino;
 
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -7,6 +10,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.TextAlignment;
+import javafx.util.Duration;
+
 import java.io.IOException;
 
 public class WheelFortController {
@@ -22,6 +27,8 @@ public class WheelFortController {
 
     //Bild des Glücksrads wird definiert
     Image wheelImg = new Image("http://www.gis-informatik.de/~torge.neuendorf/Bilder/wheelImg.png");
+    RotateTransition rotate = new RotateTransition();
+    int rotation = -9;
 
 
     //Initialisieren des Fensters
@@ -38,13 +45,24 @@ public class WheelFortController {
 
     @FXML //passiert bei Drücken auf den Startknopf
     public void spin() {
-            WheelField field = WheelFort.wheelSpin(); //Aufrufen der eigentlichen Logik
-            int rotation = field.getnumber()*(-18) - 9;
-            imgLabel.setRotate(rotation); //richtige Drehung des Rads einstellen
-            valLabel.setText("Gewinn: " + field.getValue() + "$"); //Gewinn anzeigen
-            amountLabel.setText(VirtualCasinoController.getCurrAmount() + "$"); //Neuen Kontostand anzeigen
-            if (VirtualCasinoController.getCurrAmount() < 25) {
-                spinBtn.setDisable(true); //Button ausschalten, wenn zu wenig Geld da ist
-            }
+        WheelField field = WheelFort.wheelSpin(); //Aufrufen der eigentlichen Logik
+        //Rad drehen
+        rotate.setNode(imgLabel); //Was sich drehen soll
+        rotate.setDuration(Duration.millis(2500)); //Dauer der Drehung
+        int rotationBefore = rotation; //Zur Berechnung - Winkel vorher
+        rotation = field.getnumber()*(-18) - 9; //benötigter Winkel
+        //Anpassen der Drehung (je nachdem, ob der neue Winkel größer oder kleiner als der alte ist), ca. 3 Drehungen
+        if (rotation <= rotationBefore) {
+            rotate.setByAngle(-1080 + (rotation - rotationBefore));
+        } else {
+            rotate.setByAngle(-1080 - (rotationBefore - rotation));
+        }
+        rotate.play(); //Drehung abspielen
+
+        valLabel.setText("Gewinn: " + field.getValue() + "$"); //Gewinn anzeigen
+        amountLabel.setText(VirtualCasinoController.getCurrAmount() + "$"); //Neuen Kontostand anzeigen
+        if (VirtualCasinoController.getCurrAmount() < 25) {
+            spinBtn.setDisable(true); //Button ausschalten, wenn zu wenig Geld da ist
+        }
     }
 }
